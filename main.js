b6683d187ec51db687c75d63a8009b1b1b6b3146
Marc Heinrich <marc.heinrich@free.fr>
2026-02-05 22:11:05 +0100

    Cleaning up the server side code

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
+const yaml = require('yaml');
 const app = express();
-const PORT = 8000;
 
-app.use('/letempsdalice', express.static(__dirname + '/public'));
+// Read configuration from yaml config file
+const configFile = fs.readFileSync('./config.yaml', 'utf8');
+const config = yaml.parse(configFile);
+
+const PORT = config["port"];
+const BASE_URL = config["base_url"];
+//Maximum size of the uploaded file in MB.
+const MAX_DB_SIZE = config["max_db_size"];
+
+// Make the public directory accessible
+app.use(BASE_URL, express.static(__dirname + '/public'));
 
 // Set up Multer for handling file uploads
 const upload = multer({
     dest: __dirname + '/temp',
-    limits: { fileSize: 10 * 1024 * 1024 }, // 10MB
+    limits: { fileSize: MAX_DB_SIZE * 1024 * 1024 }, //Convert MB to octets.
 });
 
 // Route for handling file uploads
-app.post('/letempsdalice/upload', upload.single('dbFile'), (req, res) => {
+app.post(BASE_URL + '/upload', upload.single('dbFile'), (req, res) => {
     if (!req.file) {
         res.status(400).send('No file part');
         return;
@@ -42,5 +52,5 @@ app.use((req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Serving at http://localhost:${PORT}`);
+    console.log(`Serving at http://localhost:${PORT}${BASE_URL}`);
 });
